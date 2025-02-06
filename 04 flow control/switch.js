69b8b7eb70b1af3c6c0967a3f2800878612d8ed4
Arjunbhaliya <167542001+Arjunbhaliya@users.noreply.github.com>
2025-02-06 12:35:20 -0700

    commit

---
+++ 04 flow control/switch.js
@@ -0,0 +1,32 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+let month = 4
+switch (month) {
+    case 1:
+        console.log("JANUARY")
+        break; 
+    case 2:
+        console.log("FEBRUARY")
+        break; 
+    case 3:
+        console.log("MARCH")
+        break; 
+    case 4:
+        console.log("APRIL")
+        break; 
+    case 4:
+        console.log("MAY")
+        break; 
+
+    default:
+        break;
+}
+
+
